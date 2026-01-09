9e08bbfd1426d82479b6f94d0d3e96afa0fc9470
Davis <davisgalla946@gmail.com>
2026-01-09 10:45:30 +0200

    docs little  fix

--- resources/views/documents/show.blade.php
+++ resources/views/documents/show.blade.php
@@ -1,5 +1,4 @@
 <x-app-layout>
-    <x-app-layout>
     <h2 class="text-2xl font-bold mb-4 text-white">Document Details</h2>
         <p class="text-gray-400"><span class="text-white">ID:</span> {{ $document->id }}</p>
         <p class="text-gray-400"><span class="text-white">Case ID:</span> {{ $document->keis_id }}</p>
@@ -9,5 +8,5 @@
         <p class="text-gray-400"><span class="text-white">pages:</span> {{ $document->pages }}</p>
         <p class="text-gray-400"><span class="text-white">Uploaded by:</span> {{ $document->uploaded_by}}</p>
         <a href="{{ route('docs.index') }}" class="inline-block mt-4 text-blue-600 hover:underline">Back to Documents</a>
-</x-app-layout>
+
 </x-app-layout>
